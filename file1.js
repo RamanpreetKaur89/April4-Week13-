b8c12d072dec5c17fba5c94664afae42c03b99f1
Ramanpreet Kaur Na <741319@student.tor.loc>
2019-04-04 11:06:55 -0400

    add numbers from 0 to 11

--- file1.js
+++ file1.js
@@ -1,7 +1,16 @@
 
 let i=0;
-while(i<=11)
+/*while(i<=11) Print numbers from 0 to 11
 {
     console.log(i)
     i++;
+}*/
+
+let sum =0;
+while(i<=11)
+{
+    sum = sum +i;
+    i++;
 }
+console.log(sum);
+
